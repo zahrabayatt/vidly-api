ad71d06ebc3f20a069386bdb99ca642b4ea01247
Zahra Bayat <zahra.bayat13799@gmail.com>
2024-09-22 14:23:41 +0330

    Add npm packages and config express

---
+++ index.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const app = express();
+
+mongoose
+  .connect("mongodb://127.0.0.1:27017/vidly")
+  .then(() => console.log("Connected to MongoDB..."))
+  .catch((err) => console.error("Couldn't connect to MongoDB...", err));
+
+app.use(express.json());
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => console.log(`Listening on port ${port}...`));
