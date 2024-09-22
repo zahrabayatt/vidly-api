09c30f0c883ed2772c4b57b15a390b3bb3d1ef28
Zahra Bayat <zahra.bayat13799@gmail.com>
2024-09-22 15:45:35 +0330

    Add the movies API

--- models/genre.js
+++ models/genre.js
@@ -20,5 +20,6 @@ function validateGenre(genre) {
   return schema.validate(genre);
 }
 
+exports.genreSchema = genreSchema;
 exports.Genre = Genre;
 exports.validate = validateGenre;
---
+++ routes/movies.js
@@ -0,0 +1,96 @@
+const { Movie, validate } = require("../models/movie");
+const { Genre } = require("../models/genre");
+const express = require("express");
+const router = express.Router();
+
+router.get("/", async (req, res) => {
+  const movies = await Movie.find().sort("title");
+  res.send(movies);
+});
+
+router.get("/:id", async (req, res) => {
+  const movie = await Movie.findById(req.params.id);
+
+  if (!movie) {
+    res.status(404).send("The movie with the given ID was not found!");
+    return;
+  }
+
+  res.send(movie);
+});
+
+router.post("/", async (req, res) => {
+  const { error } = validate(req.body);
+  if (error) {
+    res.status(400).send(error.message);
+    return;
+  }
+
+  const genre = await Genre.findById(req.body.genreId);
+  if (!genre) {
+    res.status(400).send("Invalid genre.");
+    return;
+  }
+
+  let movie = new Movie({
+    title: req.body.title,
+    // genre: genre // don't do this because the genre object has other properties like __v that we don't want to embedded in movie document
+    genre: {
+      _id: genre._id,
+      name: genre.name,
+    },
+    numberInStock: req.body.numberInStock,
+    dailyRentalRate: req.body.dailyRentalRate,
+  });
+  movie = await movie.save();
+
+  res.send(movie);
+});
+
+router.put("/:id", async (req, res) => {
+  const { error } = validate(req.body);
+  if (error) {
+    res.status(400).send(error.message);
+    return;
+  }
+
+  const genre = await Genre.findById(req.body.genreId);
+  if (!genre) {
+    res.status(400).send("Invalid genre.");
+    return;
+  }
+
+  const movie = await Movie.findByIdAndUpdate(
+    req.params.id,
+    {
+      title: req.body.title,
+      genre: {
+        _id: genre._id,
+        name: genre.name,
+      },
+      numberInStock: req.body.numberInStock,
+      dailyRentalRate: req.body.dailyRentalRate,
+    },
+    { new: true }
+  );
+
+  if (!movie) {
+    res.status(404).send("The movie with the given ID was not found!");
+    return;
+  }
+
+  res.send(movie);
+});
+
+router.delete("/:id", async (req, res) => {
+  const movie = await Movie.findByIdAndDelete(req.params.id);
+
+  if (!movie) {
+    res.status(404).send("The movie with the given ID was not found!");
+    return;
+  }
+
+  res.send(movie);
+});
+
+module.exports = router;
