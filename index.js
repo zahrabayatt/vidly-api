94d25dd100b7c025f0abffc82b526d6d4d64e539
Zahra Bayat <zahra.bayat13799@gmail.com>
2024-10-21 23:26:30 +0330

    Log errors in file using winston package

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 require("express-async-errors");
+const winston = require("winston");
 const error = require("./middlewares/error");
 const config = require("config");
 const Joi = require("joi");
@@ -12,6 +13,8 @@ const users = require("./routes/users");
 const auth = require("./routes/authentication");
 const express = require("express");
 const app = express();
+
+winston.add(new winston.transports.File({ filename: "logfile.log" }));
 
 if (!config.get("jwtPrivateKey")) {
   console.error("FATAL ERROR: jwtPrivateKey is not defined.");
--- middlewares/error.js
+++ middlewares/error.js
@@ -1,4 +1,16 @@
+const winston = require("winston");
+
 module.exports = function (err, req, res, next) {
-  // Log the exception
+  //winston.log("error", err.message);
+  // or
+  winston.error(err.message, err);
+
+  // log level:
+  // error
+  // warn
+  // info
+  // verbose
+  // debug
+  // silly
   res.status(500).send("Something failed.");
 };
