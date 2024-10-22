c1017e9be153e68c49e9435589aec3122e5fbdbf
Zahra Bayat <zahra.bayat13799@gmail.com>
2024-10-22 18:12:31 +0330

    Extracting the logging logic

--- index.js
+++ index.js
@@ -1,25 +1,12 @@
-require("express-async-errors");
-const winston = require("winston");
-require("winston-mongodb");
 const config = require("config");
 const Joi = require("joi");
 Joi.objectId = require("joi-objectid")(Joi);
 const express = require("express");
 const app = express();
 
+require("./startup/logging")();
 require("./startup/routes")(app);
 require("./startup/db")();
-
-winston.exceptions.handle(
-  new winston.transports.File({ filename: "uncaughtExceptions.log" })
-);
-
-winston.add(new winston.transports.File({ filename: "logfile.log" }));
-winston.add(
-  new winston.transports.MongoDB({
-    db: "mongodb://127.0.0.1:27017/vidly",
-  })
-);
 
 if (!config.get("jwtPrivateKey")) {
   console.error("FATAL ERROR: jwtPrivateKey is not defined.");
---
+++ startup/logging.js
@@ -0,0 +1,16 @@
+require("express-async-errors");
+const winston = require("winston");
+require("winston-mongodb");
+
+module.exports = () => {
+  winston.exceptions.handle(
+    new winston.transports.File({ filename: "uncaughtExceptions.log" })
+  );
+
+  winston.add(new winston.transports.File({ filename: "logfile.log" }));
+  winston.add(
+    new winston.transports.MongoDB({
+      db: "mongodb://127.0.0.1:27017/vidly",
+    })
+  );
+};
