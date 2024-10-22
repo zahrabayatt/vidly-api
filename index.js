56cf60436e0fad7a3d6b337ac9204c7ad7191d7a
Zahra Bayat <zahra.bayat13799@gmail.com>
2024-10-22 18:08:01 +0330

    Extracting the db logic

--- index.js
+++ index.js
@@ -4,11 +4,11 @@ require("winston-mongodb");
 const config = require("config");
 const Joi = require("joi");
 Joi.objectId = require("joi-objectid")(Joi);
-const mongoose = require("mongoose");
 const express = require("express");
 const app = express();
 
 require("./startup/routes")(app);
+require("./startup/db")();
 
 winston.exceptions.handle(
   new winston.transports.File({ filename: "uncaughtExceptions.log" })
@@ -25,11 +25,6 @@ if (!config.get("jwtPrivateKey")) {
   console.error("FATAL ERROR: jwtPrivateKey is not defined.");
   process.exit(1);
 }
-
-mongoose
-  .connect("mongodb://127.0.0.1:27017/vidly")
-  .then(() => console.log("Connected to MongoDB..."))
-  .catch((err) => console.error("Couldn't connect to MongoDB...", err));
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => console.log(`Listening on port ${port}...`));
