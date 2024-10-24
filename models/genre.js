fd0546254898e32a6d97bd89f1411047d36b04ec
Zahra Bayat <zahra.bayat13799@gmail.com>
2024-10-24 17:32:42 +0330

    Add integration test for creating a genre if the input is invalid

--- models/genre.js
+++ models/genre.js
@@ -5,7 +5,7 @@ const genreSchema = new mongoose.Schema({
   name: {
     type: String,
     required: true,
-    minLength: 3,
+    minLength: 5,
     maxLength: 50,
   },
 });
@@ -14,7 +14,7 @@ const Genre = mongoose.model("Genre", genreSchema);
 
 function validateGenre(genre) {
   const schema = Joi.object({
-    name: Joi.string().min(3).max(50).required(),
+    name: Joi.string().min(5).max(50).required(),
   });
 
   return schema.validate(genre);
--- tests/intergration/genres.test.js
+++ tests/intergration/genres.test.js
@@ -1,5 +1,6 @@
 const request = require("supertest");
 const { Genre } = require("../../models/genre");
+const { User } = require("../../models/user");
 
 let server;
 
@@ -54,6 +55,29 @@ describe("/api/genres", () => {
         .send({ name: "genre1" });
 
       expect(res.status).toBe(401);
+    });
+
+    it("should return 400 if genre's name is less than 5 character", async () => {
+      const token = new User().generateAuthToken();
+
+      const res = await request(server)
+        .post("/api/genres")
+        .set("x-auth-token", token)
+        .send({ name: "1234" });
+
+      expect(res.status).toBe(400);
+    });
+
+    it("should return 400 if genre's name is more than 50 character", async () => {
+      const token = new User().generateAuthToken();
+      const name = new Array(52).join("a");
+
+      const res = await request(server)
+        .post("/api/genres")
+        .set("x-auth-token", token)
+        .send({ name: name });
+
+      expect(res.status).toBe(400);
     });
   });
 });
