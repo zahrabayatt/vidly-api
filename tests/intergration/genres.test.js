9684d549d9e558f29cba52bdd1cef8142e320b57
Zahra Bayat <zahra.bayat13799@gmail.com>
2024-10-24 15:36:42 +0330

    Add integration test for getting genre with id - testing route with parameters

--- tests/intergration/genres.test.js
+++ tests/intergration/genres.test.js
@@ -28,4 +28,16 @@ describe("/api/genres", () => {
       expect(res.body.some((g) => g.name == "genre2")).toBeTruthy();
     });
   });
+
+  describe("Get /:id", () => {
+    it("should return a genre if valid id is passed", async () => {
+      const genre = new Genre({ name: "genre1" });
+      await genre.save();
+
+      const res = await request(server).get(`/api/genres/${genre._id}`);
+
+      expect(res.status).toBe(200);
+      expect(res.body).toHaveProperty("name", "genre1");
+    });
+  });
 });
