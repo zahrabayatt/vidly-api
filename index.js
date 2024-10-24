1b2a66635bf29ad6a2e82a375a6f4eea282ed70d
Zahra Bayat <zahra.bayat13799@gmail.com>
2024-10-24 15:05:01 +0330

    Add an integration test for getting all genres

--- index.js
+++ index.js
@@ -9,9 +9,8 @@ require("./startup/config")();
 require("./startup/validation")();
 
 const port = process.env.PORT || 3000;
-app.listen(port, () => winston.info(`Listening on port ${port}...`));
+const server = app.listen(port, () =>
+  winston.info(`Listening on port ${port}...`)
+);
 
-// to run application in test environment, run these command in cmd:
-// set vidly_jwtPrivateKey=[Your_Key]
-// set NODE_ENV=test
-// node index.js
+module.exports = server;
