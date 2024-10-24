3ac495d18176b44aa2d5d39c9ec9f4f348998973
Zahra Bayat <zahra.bayat13799@gmail.com>
2024-10-24 19:25:30 +0330

    Add integration test for updating genre

--- tests/intergration/genres.test.js
+++ tests/intergration/genres.test.js
@@ -110,4 +110,81 @@ describe("/api/genres", () => {
       expect(res.body).toHaveProperty("name", "genre1");
     });
   });
+  describe("PUT /:id", () => {
+    let token;
+    let newName;
+    let genre;
+    let id;
+
+    const exec = () => {
+      return request(server)
+        .put("/api/genres/" + id)
+        .set("x-auth-token", token)
+        .send({ name: newName });
+    };
+
+    beforeEach(async () => {
+      genre = new Genre({ name: "genre1" });
+      await genre.save();
+
+      token = new User().generateAuthToken();
+      id = genre._id;
+      newName = "updatedName";
+    });
+
+    it("should return 401 if client is not logged in", async () => {
+      token = "";
+
+      const res = await exec();
+
+      expect(res.status).toBe(401);
+    });
+
+    it("should return 400 if genre is less than 5 characters", async () => {
+      newName = "1234";
+
+      const res = await exec();
+
+      expect(res.status).toBe(400);
+    });
+
+    it("should return 400 if genre is more than 50 characters", async () => {
+      newName = new Array(52).join("a");
+
+      const res = await exec();
+
+      expect(res.status).toBe(400);
+    });
+
+    it("should return 404 if id is invalid", async () => {
+      id = 1;
+
+      const res = await exec();
+
+      expect(res.status).toBe(404);
+    });
+
+    it("should return 404 if genre with the given id was not found", async () => {
+      id = new mongoose.Types.ObjectId();
+
+      const res = await exec();
+
+      expect(res.status).toBe(404);
+    });
+
+    it("should update the genre if input is valid", async () => {
+      await exec();
+
+      const updatedGenre = await Genre.findById(genre._id);
+
+      expect(updatedGenre.name).toBe(newName);
+    });
+
+    it("should return the updated genre if it is valid", async () => {
+      const res = await exec();
+
+      expect(res.body).toHaveProperty("_id");
+      expect(res.body).toHaveProperty("name", newName);
+    });
+  });
 });
