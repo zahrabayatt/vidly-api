e6f622680bf27484d2b7de6a5bac8283053c2b69
Zahra Bayat <zahra.bayat13799@gmail.com>
2024-10-25 18:39:23 +0330

    Add integration test for returns post api to test the input

--- routes/returns.js
+++ routes/returns.js
@@ -1,7 +1,23 @@
+const Joi = require("joi");
 const authorization = require("../middlewares/authorization");
 const express = require("express");
 const router = express.Router();
 
-router.post("/", authorization, (req, res) => {});
+router.post("/", authorization, (req, res) => {
+  const { error } = validate(req.body);
+  if (error) {
+    res.status(400).send(error.message);
+    return;
+  }
+});
+
+function validate(param) {
+  const schema = Joi.object({
+    customerId: Joi.objectId().required(),
+    movieId: Joi.objectId().required(),
+  });
+
+  return schema.valid(param);
+}
 
 module.exports = router;
--- tests/intergration/returns.test.js
+++ tests/intergration/returns.test.js
@@ -1,6 +1,6 @@
 const request = require("supertest");
 const { Rental } = require("../../models/rental");
-const { default: mongoose } = require("mongoose");
+const mongoose = require("mongoose");
 const { User } = require("../../models/user");
 // Structure:
 // POST /api/returns {customerId, movieId}
@@ -68,6 +68,22 @@ describe("/api/returns", () => {
       const res = await exec();
 
       expect(res.status).toBe(401);
+    });
+
+    it("should return 400 if customerId is not provided", async () => {
+      customerId = "";
+
+      const res = await exec();
+
+      expect(res.status).toBe(400);
+    });
+
+    it("should return 400 if movieId is not provided", async () => {
+      movieId = "";
+
+      const res = await exec();
+
+      expect(res.status).toBe(400);
     });
   });
 });
